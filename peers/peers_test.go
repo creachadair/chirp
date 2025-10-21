9a0567a842d19f60589e80317e32518b561bec61
M. J. Fromberger <michael.j.fromberger@gmail.com>
2025-10-20 19:30:05 -0700

    peers: use mnet.Network so tests can run in synctest
    Drop the fake network fixtures and use mnet.Nework throughout.  This allows the
    Loop test to use the synctest managed clock instead of having to really wait.
    With that, also increase the number of calls spammed, and distribute the waits
    with a random number to give the race detector more to work with.

--- peers/peers_test.go
+++ peers/peers_test.go
@@ -4,7 +4,7 @@ package peers_test
 
 import (
 	"context"
-	"errors"
+	mrand "math/rand/v2"
 	"net"
 	"testing"
 	"testing/synctest"
@@ -13,70 +13,42 @@ import (
 	"github.com/creachadair/chirp"
 	"github.com/creachadair/chirp/channel"
 	"github.com/creachadair/chirp/peers"
+	"github.com/creachadair/mds/mnet"
 	"github.com/creachadair/taskgroup"
-	"github.com/fortytw2/leaktest"
 )
 
-func mustListen(t *testing.T) (_ net.Listener, addr string) {
+func newTestNetwork(t *testing.T) *mnet.Network {
+	n := mnet.New(t.Name() + " network")
+	t.Cleanup(func() { n.Close() })
+	return n
+}
+
+func mustListen(t *testing.T, n *mnet.Network) net.Listener {
 	t.Helper()
-	lst, err := net.Listen("tcp", "127.0.0.1:0")
+	lst, err := n.Listen("test", t.Name())
 	if err != nil {
-		t.Fatalf("Listen: %v", err)
+		t.Fatalf("Listen failed: %v", err)
 	}
-	addr = lst.Addr().String()
 	t.Cleanup(func() { lst.Close() })
-	t.Logf("Listening at %q", addr)
-	return lst, addr
+	return lst
 }
-
-type fakeListener struct {
-	net.Listener // stub for unused methods
-	conns        chan net.Conn
-	closed       chan struct{}
-}
-
-func (f fakeListener) push(c net.Conn) { f.conns <- c }
-
-func (f fakeListener) Accept() (net.Conn, error) {
-	select {
-	case <-f.closed:
-		return nil, net.ErrClosed
-	case c := <-f.conns:
-		return c, nil
-	}
-}
-
-func (f fakeListener) Close() error {
-	select {
-	case <-f.closed:
-		return net.ErrClosed
-	default:
-		close(f.closed)
-		return nil
-	}
-}
-
-func newFakeListener() fakeListener {
-	return fakeListener{
-		conns:  make(chan net.Conn),
-		closed: make(chan struct{}),
-	}
-}
-
-// fakeConn is a fake implementation of [net.Conn] that does not work but which
-// satisfies the interface, for use in testing. Only the Close method can be
-// called without panicking.
-type fakeConn struct{ net.Conn }
-
-func (fakeConn) Close() error { return nil }
 
 func TestAccepter(t *testing.T) {
 	t.Run("OK", func(t *testing.T) {
 		synctest.Test(t, func(t *testing.T) {
-			lst := newFakeListener()
+			n := newTestNetwork(t)
+			lst := mustListen(t, n)
 			acc := peers.NetAccepter(lst)
 
-			time.AfterFunc(1*time.Second, func() { lst.push(fakeConn{}) })
+			time.AfterFunc(1*time.Second, func() {
+				conn, err := n.DialContext(t.Context(), lst.Addr().Network(), lst.Addr().String())
+				if err != nil {
+					t.Errorf("Dial failed: %v", err)
+				}
+				t.Logf("Dial OK: %v", conn.RemoteAddr())
+				conn.Close()
+			})
+
 			c, err := acc.Accept(t.Context())
 			if err != nil {
 				t.Fatalf("Accept: unexpected error: %v", err)
@@ -94,8 +66,10 @@ func TestAccepter(t *testing.T) {
 
 	t.Run("Cancel", func(t *testing.T) {
 		synctest.Test(t, func(t *testing.T) {
-			lst := newFakeListener()
+			n := newTestNetwork(t)
+			lst := mustListen(t, n)
 			acc := peers.NetAccepter(lst)
+
 			ctx, cancel := context.WithTimeout(t.Context(), 5*time.Second)
 			defer cancel()
 
@@ -103,59 +77,56 @@ func TestAccepter(t *testing.T) {
 			if err == nil {
 				t.Errorf("Accept: got %v, want error", ch)
 			}
-
-			// The listener should already be closed, so this should report that error.
-			if err := lst.Close(); !errors.Is(err, net.ErrClosed) {
-				t.Errorf("Close listener: got %v, want %v", err, net.ErrClosed)
-			}
 		})
 	})
 }
 
 func TestLoop(t *testing.T) {
-	defer leaktest.Check(t)()
+	synctest.Test(t, func(t *testing.T) {
+		n := newTestNetwork(t)
+		lst := mustListen(t, n)
 
-	lst, addr := mustListen(t)
-	ctx, cancel := context.WithCancel(t.Context())
-	defer cancel()
+		ctx, cancel := context.WithCancel(t.Context())
+		defer cancel()
 
-	base := chirp.NewPeer().Handle("100", slowEcho)
-	loop := taskgroup.Go(func() error {
-		return peers.Loop(ctx, peers.NetAccepter(lst), base.Clone)
-	})
-	t.Log("Started peer loop...")
-
-	const numClients = 5
-	const numCalls = 5
-	t.Logf("Clients: %d, calls per client: %d", numClients, numCalls)
-
-	g := taskgroup.New(func(err error) {
-		cancel()
-		t.Errorf("Task error: %v", err)
-	})
-	for range numClients {
-		g.Go(func() error {
-			conn, err := net.Dial("tcp", addr)
-			if err != nil {
-				return err
-			}
-			defer conn.Close()
-			peer := chirp.NewPeer().Start(channel.IO(conn, conn))
-			for j := range numCalls {
-				_, err := peer.Call(t.Context(), "100", nil)
-				if err != nil {
-					t.Errorf("Call %d: %v", j+1, err)
-				}
-			}
-			return peer.Stop()
+		base := chirp.NewPeer().Handle("100", slowEcho)
+		loop := taskgroup.Go(func() error {
+			return peers.Loop(ctx, peers.NetAccepter(lst), base.Clone)
 		})
-	}
-	t.Logf("Clients finished, err=%v", g.Wait())
-	t.Logf("Closed listener, err=%v", lst.Close())
-	t.Logf("Loop exited, err=%v", loop.Wait())
+		t.Log("Started peer loop...")
+
+		const numClients = 10
+		const numCalls = 100
+		t.Logf("Clients: %d, calls per client: %d", numClients, numCalls)
+
+		g := taskgroup.New(func(err error) {
+			cancel()
+			t.Errorf("Task error: %v", err)
+		})
+		for range numClients {
+			g.Go(func() error {
+				conn, err := n.Dial(lst.Addr().Network(), lst.Addr().String())
+				if err != nil {
+					return err
+				}
+				defer conn.Close()
+				peer := chirp.NewPeer().Start(channel.IO(conn, conn))
+				for j := range numCalls {
+					_, err := peer.Call(t.Context(), "100", nil)
+					if err != nil {
+						t.Errorf("Call %d: %v", j+1, err)
+					}
+				}
+				return peer.Stop()
+			})
+		}
+		t.Logf("Clients finished, err=%v", g.Wait())
+		t.Logf("Closed listener, err=%v", lst.Close())
+		t.Logf("Loop exited, err=%v", loop.Wait())
+	})
 }
 
 func slowEcho(ctx context.Context, req *chirp.Request) ([]byte, error) {
-	time.Sleep(7 * time.Millisecond)
+	time.Sleep((mrand.N[time.Duration](500) + 500) * time.Millisecond)
 	return req.Data, nil
 }
